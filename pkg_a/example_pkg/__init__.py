1c0c4870ffb096ece7a652e94dbc7bffa4987d9d
rickstaa <rick.staa@outlook.com>
2023-06-27 10:09:43 +0200

    feat: add bug trigger code

---
+++ pkg_a/example_pkg/__init__.py
@@ -0,0 +1,4 @@
+# Make module version available.
+from example_pkg.a._version import version as __version__  # noqa: F401
+from example_pkg.a._version import \
+    version_tuple as __version_tuple__  # noqa: F401
---
+++ pkg_b/example_pkg/__init__.py
@@ -0,0 +1,4 @@
+# Make module version available.
+from example_pkg.b._version import version2 as __version__2  # noqa: F401
+from example_pkg.b._version import \
+    version_tuple2 as __version_tuple__2  # noqa: F401
